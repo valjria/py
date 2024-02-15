0bb5303044310d2c5f06416f7ff40e389761de30
Barış Mumyakmaz <58375899+valjria@users.noreply.github.com>
2024-02-15 19:40:13 +0300

    Update exploratory_data_analysis.py

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -203,3 +203,97 @@ for col in categoric_cols:
 
 for col in num_cols:
     num_summary(df, col, plot=True)
+
+#4. HEDEF DEĞİŞKEN ANALİZİ ( ANALYSIS OF TARGET VARIABLE)
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+
+for col in df.columns:
+    if df[col].dtypes == "bool":
+        df[col]= df[col].astype(int)
+
+def cat_summary(dataframe, col_name, plot = False ):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts( ) / len(dataframe)}))
+    print("###############################################")
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    #docstring ekle
+
+    categoric_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+    num_but_categoric = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int64", "float64"]]
+    cat_but_car = [col for col in df.columns if
+                   df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+    categoric_cols = categoric_cols + num_but_categoric
+    categoric_cols = [col for col in categoric_cols if col not in cat_but_car]
+
+    num_cols = [col for col in df.columns if df[col].dtypes in ["int64", "float64"]]
+    num_cols = [col for col in num_cols if col not in categoric_cols]
+
+    print(f"Observations:   {dataframe.shape[0]}")
+    print(f"Variables:   {dataframe.shape[1]}")
+    print(f"categoric_cols:   {len(categoric_cols)}")
+    print(f"num_cols:   {len(num_cols)}")
+    print(f"cat_but_car:   {len(cat_but_car)}")
+    print(f"num_but_categoric:   {len(num_but_categoric)}")
+
+    return categoric_cols, num_cols, cat_but_car
+
+categoric_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+df.head()
+
+cat_summary(df,"survived")
+
+#HEDEF DEĞİŞKENİN KATEGORİK DEĞİŞKENLER İLE ANALİZİ
+
+df.groupby("sex")["survived"].mean()
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}))
+
+target_summary_with_cat(df, "survived","sex")
+target_summary_with_cat(df, "survived","pclass")
+
+for col in categoric_cols:
+    target_summary_with_cat(df, "survived", col)
+
+#HEDEF DEĞİŞKENİN SAYISAL DEĞİŞKENLERLE ANALİZİ
+
+df.groupby("survived")["age"].mean()
+
+df.groupby("sex")["survived"].mean()
+
+df.groupby("survived").agg({"age":"mean"})
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col:"mean"}), end = "\n\n\n")
+
+target_summary_with_num(df,"survived","age")
+
+for col in num_cols:
+    target_summary_with_num(df,"sex",col)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
